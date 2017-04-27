2cd36994310befc853bcc74469a573b664a22f9a
Alessandro Moscatelli <moskante@gmail.com>
2017-04-27 23:14:19 +0200

    Modified psych.function Package renamed to MixedPsych

--- R/psych.function.R
+++ R/psych.function.R
@@ -1,17 +1,7 @@
 #psych.function fit a glm to binary data and plot the fitted function
-#AM did it on 25.07.2013
-#AM modified on 30.07.2015: PSE and JND in the outcome
-#To do: multivariable model plotting
-
-#ps.formula		formula object such as as.formula("cbind(away, towards) ~ velocity")
-#ps.link		character. link function
-#ps.data  	data.frame including the binary response variables and predictor
-#x.range		the range over which the function will be plotted.
-#ps.col     character. Color of the line
-#ps.lines   logical, indicatinf if a line of the model fit should be added to the existing plot 
-
 psych.function = function(ps.formula, ps.link, ps.data, x.range = c(NA, NA), ps.x = NA, 
-                          ps.lines = F, ps.col = "black", br = F){
+                          ps.lines = F, ps.col = "black", ps.lty = "dashed",
+                          br = F){
 	myfit = vector("list", 2)
   ps.terms = terms(ps.formula)
   
@@ -30,7 +20,8 @@ psych.function = function(ps.formula, ps.link, ps.data, x.range = c(NA, NA), ps.
   if(ps.link == "probit"){
     myfit[[2]] = delta.psy.probit(model.glm)
     if(ps.lines == T){
-      segments(x0 = myfit[[2]][1,3], y0 = 0.5, x1 = myfit[[2]][1,4], y1 = 0.5, col = ps.col)
+      segments(x0 = myfit[[2]][1,3], y0 = 0.5, x1 = myfit[[2]][1,4], y1 = 0.5,
+               col = ps.col, lty = ps.lty)
     }
   }else{
     myfit[[2]] = NA
@@ -44,7 +35,7 @@ psych.function = function(ps.formula, ps.link, ps.data, x.range = c(NA, NA), ps.
     }
 		lines(x = seq(x.range[1], x.range[2], length.out = 1000),
 		 y = predict(object = model.glm, newdata = data.frame(ps.x), type = "response"),
-		 col = ps.col)
+		 col = ps.col, lty = ps.lty)
 		}
 	return(myfit)
 	}
