e1dc59e70e473d166decfdbcb31c0c17f33b56f4
Alessandro Moscatelli <moskante@gmail.com>
2017-04-29 14:28:07 +0200

    Modified psych.shape*

--- R/psych.shape.R
+++ R/psych.shape.R
@@ -1,19 +1,3 @@
-#psych.shape() plot a psychometric function of a knwonwn shape and location.
-#The function always requires an existing plot (add = TRUE)
-#Arguments
-#pse, jnd  the pse and the jnd of the desired psychometric function
-#x.range  the range over which the function will be plotted.
-#ps.link		character. link function
-#ps.col     character. Color of the fitted function
-#ps.lwd     line width
-#Example
-#y = c(0,1)
-#x = c(-40, 40)
-#plot(y ~ x, type = "n", bty = "n", lab = c(5,3,7))
-#psych.shape(pse = 0, jnd = 6, x.range = c(-40, 40), ps.col = "gray", ps.lwd = 3)
-#psych.shape(pse = 6, jnd = 6, x.range = c(-40, 40), ps.col = "black")
-#psych.shape(pse = 6, jnd = 6, x.range = c(-40, 40), ps.col = "red", ps.link = "logit", ps.lwd = 3)
-
 psych.shape = function(pse = 0, jnd, x.range = c(NA, NA),  ps.link = "probit", ps.col = "black",
                        ps.lwd = 1){
   if(ps.link == "probit"){
