a57968e69d2916a04568e35a356601c3cc88e7c8
Priscilla <priscillabalestrucci@gmail.com>
2017-05-18 15:48:21 +0200

    formatR

--- R/psych.function.R
+++ R/psych.function.R
@@ -1,41 +1,40 @@
-#psych.function fit a glm to binary data and plot the fitted function
-psych.function = function(ps.formula, ps.link, ps.data, x.range = c(NA, NA), ps.x = NA, 
-                          ps.lines = F, ps.col = "black", ps.lty = "dashed",
-                          br = F){
-	myfit = vector("list", 2)
-  ps.terms = terms(ps.formula)
-  
-  model.glm = glm(formula = ps.formula, family = binomial(link = ps.link), data = ps.data)
-  brflag = ifelse(trunc(max(model.glm$fitted.values)) == 1 | trunc(min(model.glm$fitted.values)) == 0, T, F)
-  myfit[[3]] = brflag
-  
-  if(br == T & brflag == T){
-    require(brglm)
-    model.glm = brglm(formula = ps.formula, family = binomial(link = ps.link), data = ps.data)
-    warning("Binomial-response GLMs fitted using the bias-reduction method (brglm)")
-  }
-	
-  myfit[[1]] = model.glm
-  
-  if(ps.link == "probit"){
-    myfit[[2]] = psych.delta(model.glm)
-    if(ps.lines == T){
-      segments(x0 = myfit[[2]][1,3], y0 = 0.5, x1 = myfit[[2]][1,4], y1 = 0.5,
-               col = ps.col, lty = ps.lty)
+# psych.function fit a glm to binary data and plot the fitted function
+psych.function = function(ps.formula, ps.link, ps.data, x.range = c(NA, NA), ps.x = NA, ps.lines = F, 
+    ps.col = "black", ps.lty = "dashed", br = F) {
+    myfit = vector("list", 2)
+    ps.terms = terms(ps.formula)
+    
+    model.glm = glm(formula = ps.formula, family = binomial(link = ps.link), data = ps.data)
+    brflag = ifelse(trunc(max(model.glm$fitted.values)) == 1 | trunc(min(model.glm$fitted.values)) == 
+        0, T, F)
+    myfit[[3]] = brflag
+    
+    if (br == T & brflag == T) {
+        require(brglm)
+        model.glm = brglm(formula = ps.formula, family = binomial(link = ps.link), data = ps.data)
+        warning("Binomial-response GLMs fitted using the bias-reduction method (brglm)")
     }
-  }else{
-    myfit[[2]] = NA
-    warning("Use the probit link function to get the estimate of the PSE and the JND")
-  }
-  
-	if(ps.lines == T){
-    if(is.na(ps.x)){
-      ps.x <- data.frame(seq(x.range[1], x.range[2], length.out = 1000))
-      names(ps.x) = attr(ps.terms, "term.labels")
+    
+    myfit[[1]] = model.glm
+    
+    if (ps.link == "probit") {
+        myfit[[2]] = psych.delta(model.glm)
+        if (ps.lines == T) {
+            segments(x0 = myfit[[2]][1, 3], y0 = 0.5, x1 = myfit[[2]][1, 4], y1 = 0.5, col = ps.col, 
+                lty = ps.lty)
+        }
+    } else {
+        myfit[[2]] = NA
+        warning("Use the probit link function to get the estimate of the PSE and the JND")
     }
-		lines(x = seq(x.range[1], x.range[2], length.out = 1000),
-		 y = predict(object = model.glm, newdata = data.frame(ps.x), type = "response"),
-		 col = ps.col, lty = ps.lty)
-		}
-	return(myfit)
-	}
+    
+    if (ps.lines == T) {
+        if (is.na(ps.x)) {
+            ps.x <- data.frame(seq(x.range[1], x.range[2], length.out = 1000))
+            names(ps.x) = attr(ps.terms, "term.labels")
+        }
+        lines(x = seq(x.range[1], x.range[2], length.out = 1000), y = predict(object = model.glm, 
+            newdata = data.frame(ps.x), type = "response"), col = ps.col, lty = ps.lty)
+    }
+    return(myfit)
+}
--- R/psych.shape.R
+++ R/psych.shape.R
@@ -1,17 +1,16 @@
-psych.shape = function(pse = 0, jnd, x.range = c(NA, NA),  ps.link = "probit", ps.col = "black",
-                       ps.lwd = 1){
-  if(ps.link == "probit"){
-    slope = qnorm(0.75) * (1/jnd)
-    curve(expr = pnorm(x, mean = pse, sd = 1/slope), from = x.range[1], to = x.range[2], col = ps.col,
-          add = TRUE, lwd = ps.lwd)
-  }else{
-    if(ps.link == "logit"){
-      slope = log(3) * (1/jnd)
-      curve(expr = plogis(x, location = pse, scale = 1/slope), from = x.range[1], to = x.range[2], col = ps.col,
+psych.shape = function(pse = 0, jnd, x.range = c(NA, NA), ps.link = "probit", ps.col = "black", ps.lwd = 1) {
+    if (ps.link == "probit") {
+        slope = qnorm(0.75) * (1/jnd)
+        curve(expr = pnorm(x, mean = pse, sd = 1/slope), from = x.range[1], to = x.range[2], col = ps.col, 
             add = TRUE, lwd = ps.lwd)
-    }else{
-    warning("For the moment it only works with probit and logit link function")
-      }
+    } else {
+        if (ps.link == "logit") {
+            slope = log(3) * (1/jnd)
+            curve(expr = plogis(x, location = pse, scale = 1/slope), from = x.range[1], to = x.range[2], 
+                col = ps.col, add = TRUE, lwd = ps.lwd)
+        } else {
+            warning("For the moment it only works with probit and logit link function")
+        }
     }
-  return(0)
-  }
+    return(0)
+}
