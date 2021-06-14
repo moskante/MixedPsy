b42bb91aac5139c1d1ec407fad494840fe43cf5b
Priscilla Balestrucci <priscillabalestrucci@gmail.com>
2021-06-14 10:50:26 +0200

    delete files

--- reinstall.R
+++
@@ -1,8 +0,0 @@
-library(devtools)
-wd <- "/Users/priscilla/Documents/Documents - Priscilla’s MacBook Pro/repositories/MixedPsy"
-setwd(wd)
-document()
-setwd("..")
-install("MixedPsy")
-setwd(wd)
-library(MixedPsy)
