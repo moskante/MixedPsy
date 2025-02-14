6ec4d1de4fc365b37143fef6f1e7d874655f786f
Priscilla Balestrucci <priscillabalestrucci@gmail.com>
2025-02-14 13:05:19 +0100

    changes for global variable

--- R/global.R
+++ R/global.R
@@ -1,33 +1,6 @@
-utils::globalVariables(c("x_values"))
-x_values <- NULL
-#' Internal Function: Set global variable
-#' 
-#' Setting a global variable is necessary in order to correctly define the function to fit with gnlm. 
-#' 
-#' @param data dataset
-#' @param stimuli A string of the stumuli variable.
-#' 
-#' @seealso [PsychFunction_gnlm()]
-#' @noRd
-#' @return Invisible NULL.
+#' My Global Variable
 #'
-#' @keywords internal
-
-setGlobalVar <- function(data, stimuli) {
-  #unlockBinding("x_values", globalenv())
-  #globalenv()$x_values <- data[, stimuli]
-  #assign("x_values", data[, stimuli], envir = .GlobalEnv)
-  #x_values <<- data[, stimuli]
-  # Lock the binding after modification
-  #lockBinding("x_values", globalenv())
-  #invisible(NULL)
-}
-
-#' Internal Function: Delete global variable
-#' 
-#' Remove the global variable
-#' 
-#' 
-rmGlobalVar <- function(){
- rm("x_values", envir = .GlobalEnv)
-}
+#' Description of my_global_var.
+#'
+#' @name x_values
+x_values <- NULL
